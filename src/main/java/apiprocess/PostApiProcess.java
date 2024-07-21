e60a6dfdb3de2beb53152402ff2be410df495d23
mjmj <violet_prog@naver.com>
2024-07-21 12:55:32 +0900

    [feature] 이미지 저장을 위한 로직 작성

--- src/main/java/apiprocess/PostApiProcess.java
+++ src/main/java/apiprocess/PostApiProcess.java
@@ -3,21 +3,69 @@ package apiprocess;
 
 import db.H2Database;
 import enums.HttpCode;
+import enums.HttpMethod;
 import exception.SQLRuntimeException;
+import model.Part;
 import model.Post;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.FileLoader;
+import utils.Validation;
 import webserver.Request;
 import webserver.Response;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
 
 public class PostApiProcess implements ApiProcess {
+
+  private static final Logger logger = LoggerFactory.getLogger(PostApiProcess.class);
   @Override
   public String process(Request request, Response response, Map<String, Object> model) {
-    String title = request.getParameter("title");
-    String content = request.getParameter("content");
+    Part title = request.getPart("title");
+    Part content = request.getPart("content");
+    Part image = request.getPart("image");
+
+    if(Validation.anyNull(title, content)) {
+      response.setHttpCode(HttpCode.BAD_REQUEST);
+      model.put("articleFail", "제목과 글 내용 중 하나 이상이 비었습니다.");
+      return "article/index";
+    }
+
+    String titleData = new String(title.getRawBody());
+    String contentData = new String(content.getRawBody());
+
+    String uuid = UUID.randomUUID().toString();
+
+    byte[] imageData = image.getRawBody();
+    String fileName = image.getFileName();
+    String filePath = null;
+    String uploadFileName = null;
+
+    if(!fileName.isEmpty()) {
+      // 저장할 파일 경로
+      uploadFileName = uuid + "_" + fileName;
+      filePath = FileLoader.FILE_DEFAULT_PATH + "/" + uploadFileName;
+
+      try (FileOutputStream fos = new FileOutputStream(filePath)) {
+        fos.write(imageData);
+        logger.debug("이미지 파일 저장 완료: {}", uploadFileName);
+      } catch (IOException e) {
+        logger.debug("이미지 파일 저장 실패: {}", e.getMessage());
+        response.setHttpCode(HttpCode.BAD_REQUEST);
+        model.put("articleFail", "게시글 작성에 실패하셨어요.");
+        return "article/index";
+      }
+    }
+
+
     H2Database repository = H2Database.getInstance();
-    Post post = new Post(title, content);
+    Post post = new Post(titleData, contentData, uploadFileName);
     repository.addPost(post);
     response.setHttpCode(HttpCode.Found);
     response.setLocation("/");
